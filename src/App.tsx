a76ac923321049d54013baa7b173adf004b09e7c
khalidshah123 <khalid.shah@actualpixel.in>
2023-10-05 16:41:00 +0530

    added gitattributes

--- src/App.tsx
+++ src/App.tsx
@@ -2,10 +2,6 @@ import "./App.css";
 import Home from "./pages/Home";
 
 function App() {
-  // eslint-disable-next-line no-console
-  const isBoolean = true;
-  if (isBoolean) console.log("erro");
-
   return (
     <div className="App">
       <Home />
